0db91da86cec40ad675b4fc4da645026b18a5f5a
Andrey Luzin <andrey.luzin@gmail.com>
2023-11-03 04:29:32 +0600

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   basePath: "",
-  images: {
-    domains: [
-      "localhost",
-    ],
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
